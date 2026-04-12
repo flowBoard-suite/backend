4d81ba3e71536d3156dde618257844ff30a3021b
Bartosz Sosnica <bartoszsosnica@gmail.com>
2026-04-12 17:26:42 +0200

    feat: remove redundant error handling

--- src/main/java/pl/flow/board/backend/service/ActivityService.java
+++ src/main/java/pl/flow/board/backend/service/ActivityService.java
@@ -2,6 +2,7 @@ package pl.flow.board.backend.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
 import pl.flow.board.backend.model.Activity;
 import pl.flow.board.backend.repository.ActivityRepository;
 import reactor.core.publisher.Flux;
@@ -22,8 +23,7 @@ public class ActivityService {
     }
 
     public Mono<Activity> getActivityById(String activityId) {
-        return activityRepository.findById(activityId)
-                .switchIfEmpty(Mono.error(new RuntimeException("Activity not found with id: " + activityId)));
+        return activityRepository.findById(activityId);
     }
 
     public Mono<Void> deleteActivity(String activityId) {
